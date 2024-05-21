c5d9ae206e1558a352e49d9db20b1d93580bdc3d
Liurs-CM <1477045915@qq.com>
2024-05-22 00:41:22 +0800

    data_matrix_pc add pc minus one logic

--- LC_3/view/de/mdl/data_matrix_pc_load.v
+++ LC_3/view/de/mdl/data_matrix_pc_load.v
@@ -5,7 +5,7 @@
 // Email         : liurs@njust.edu.cn
 // Website       : liurs.cn
 // Created On    : 2024/05/17 22:01
-// Last Modified : 2024/05/19 23:56
+// Last Modified : 2024/05/22 00:39
 // File Name     : data_matrix_pc_load.v
 // Description   :
 //         
@@ -22,31 +22,34 @@
 module data_matrix_pc_load(/*autoarg*/
     //Inputs
     bus, ea, ld_pc, pc_sel, 
-    gate_pc, 
+    gate_pc_en, 
+    gate_pc_minus_1_en, 
     //Outputs
-    reg_pc, pc
+    reg_pc, pc, pc_minus_one
 );
 input [15:0] bus;
 input [15:0] ea;
 input ld_pc;
 input [1:0] pc_sel;
-input gate_pc;
+input gate_pc_en;
+input gate_pc_minus_1_en;
 output [15:0] reg_pc;
 output [15:0] pc;
+output [15:0] pc_minus_one;
 
 /*autodef*/
-    //Start of automatic define
+//Start of automatic define
 //Start of automatic reg
 //Define flip-flop registers here
-reg  [15:0]                     reg_pc                          ;
 //Define combination registers here
+//REG_DEL: Register reg_pc has been deleted.
 //End of automatic reg
 //Start of automatic wire
 //Define assign wires here
 wire [15:0]                     pc_mux                          ;
 //Define instance wires here
 //End of automatic wire
-    //End of automatic define
+//End of automatic define
 
 assign pc_mux[15:0] = (pc_sel[1:0]==2'b00) ? reg_pc[15:0] + 1'b1 :
                       (pc_sel[1:0]==2'b01) ? bus[15:0] :
@@ -61,7 +64,8 @@ always@(posedge clk or negedge rst_n) begin
     end
 end
 
-assign pc[15:0] = gate_pc ? reg_pc[15:0] : 16'hzzzz;
+assign pc[15:0] = gate_pc_en ? reg_pc[15:0] : 16'hzzzz;
+assign pc_minus_one[15:0] = gate_pc_minus_1_en ? (reg_pc[15:0]-1'b1) : 16'hzzzz;
 
 endmodule
 //Instance .
