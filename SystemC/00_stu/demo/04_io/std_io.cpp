82af67e6211a72417bea7623c6b32a22b121ecf6
Liurs-CM <1477045915@qq.com>
2025-06-13 20:58:54 +0800

    add systemc study examples

---
+++ SystemC/00_stu/demo/04_io/std_io.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+main(){
+    float f1, f2;
+    cout << "Enter 2 num sep by blanks: ";
+    cin >> f1 >> f2;
+    cout <<"You entered " << f1 << " " << f2 << endl;
+    return 0;
+}
